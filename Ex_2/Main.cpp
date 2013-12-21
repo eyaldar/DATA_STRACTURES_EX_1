c6c5dc7e460fe3ff4efddc254704987b6911aa07
eyaldar <eyaldar@gmail.com>
2013-12-21 21:29:09 +0200

    Ex_2 first check-in

---
+++ Ex_2/Main.cpp
@@ -0,0 +1,33 @@
+// Main.cpp : main project file.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#define INPUT_FILE "input.txt"
+#define OUTPUT_FILE "output.txt"
+
+// ==================================================================================
+//
+//		    Programming Exercise No. 2
+//		    ===========================
+//		Programmer:		Eyal Darshan
+//		Identity no.:	200898435
+//		Group:			Maof
+//		Lecturer:		Iris Gaber
+//
+// ==================================================================================
+void main()
+{
+	try
+	{
+	}
+	catch(const char* error)
+	{
+		cerr << "Error! " << error;
+	}
+	catch(...)
+	{
+		cerr << "Error!";
+	}
+}
